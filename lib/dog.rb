1bc5c99ddc867f9f31a795aef02e45215b6e753a
yaoyu bao <yaobao5354@gmail.com>
2019-08-29 06:10:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,9 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name)
     @name = name
-    @breed = dogs_breed 
   end 
   
   def name 
     @name
-  end 
-  
-  def name=(dogs_name)
-    @name = dogs_name
-  end 
-  
-  
-  def breed=(dogs_breed)
-    @breed = dogs_breed
-  end 
-  
-  def breed 
-    @breed
   end 
 end 
