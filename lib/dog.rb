db157be431fd0af73f675c716d415169c9d4028d
yaoyu bao <yaobao5354@gmail.com>
2019-08-29 06:06:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,18 +5,18 @@ class Dog
   end 
   
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    @name = dogs_name
   end 
   
   def name 
-    @this_dogs_name
+    @name
   end 
   
   def breed=(dogs_breed)
-    @this_dogs_breed = dogs_breed
+    @breed = dogs_breed
   end 
   
   def breed 
-    @this_dogs_breed
+    @breed
   end 
 end 
